2e795e9640fafc732739b9a4657dc531c1f15960
gabriel <gabrielqwes123@gmail.com>
2022-01-23 22:11:41 -0400

    I miss the method print

--- tree.js
+++ tree.js
@@ -13,17 +13,36 @@ class Tree{
     }
 
     remove(name){
-        this.children.forEach((child,index)=>{
-            console.log(child);
-            if(child.name === name) return this.children.splice(index,1)
+        this.children.forEach((child,index) =>{
+            if(child.name === name){
+                this.children.splice(index,1)
+            }
         })
+    }
+
+    getChild(name){
+        for(const child of this.children){
+            if(child.name === name) return child
+            const result = child.getChild(name)
+            if(!result) continue
+            return result
+        }
         return false
+    }
+
+    travaser(cb){
+        for(const child of this.children){
+            cb(child)
+            child.travaser(cb)
+        }
     }
 
 }
 
 const tree = new Tree("root")
 const node1 = tree.add("node1")
-const node2 = node1.add("node2")
+const node2 = tree.add("node2")
 const node3 = node2.add("node3")
-console.log(tree.remove("node1"));
+const node4 = node3.add("node4")
+
+console.log(tree.getChild("node4"));
